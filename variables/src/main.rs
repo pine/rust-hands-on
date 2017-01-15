aba3ce38b0dcc576eb92a0ae7ecf261486094bcd
Pine Mizune <pinemz@gmail.com>
2017-01-15 14:00:54 +0900

    feat(variables): add variables sample code

---
+++ variables/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let name = "world";
+    println!("Hello, {}!", name);
+
+    let count = 0;
+    let count = count + 1;
+    println!("{}", count);
+
+    let mut count = 0;
+    count += 1;
+    println!("{}", count);
+
+    let count = "#";
+    println!("{}", count);
+}
