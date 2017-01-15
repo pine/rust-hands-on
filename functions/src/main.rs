39b42f62c0be3a2acde4b28b44dfe1a8cf7d1e98
Pine Mizune <pinemz@gmail.com>
2017-01-15 14:08:59 +0900

    feat(functions): add function sample

---
+++ functions/src/main.rs
@@ -0,0 +1,12 @@
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn sub(a: i32, b: i32) -> i32 {
+    a - b
+}
+
+fn main() {
+    println!("add: {}", add(3, 5));
+    println!("sub: {}", sub(3, 5));
+}
